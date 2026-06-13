7b300a58defaefd48df904f20cda673041251469
ruleoftwo <38284207+ruleoftwo@users.noreply.github.com>
2026-06-13 13:14:50 +0300

    Add resolveVersion for version testing

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+)
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Println(resolveVersion())
+		return
+	}
 	fmt.Println("hello from gh-test-repo!")
+}
+
+func resolveVersion() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "dev"
+	}
+	if info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+	for _, s := range info.Settings {
+		if s.Key == "vcs.revision" && len(s.Value) >= 7 {
+			return "dev-" + s.Value[:7]
+		}
+	}
+	return "dev"
 }
